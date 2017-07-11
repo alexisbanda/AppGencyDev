b65448100d0d5377ac8333bef4bdbda496c8145c
ecuadorsoul <christian.alexis.banda@gmail.com>
2017-07-11 10:58:24 -0500

    first commit

---
+++ setting.php
@@ -0,0 +1,23 @@
+<?php 
+define('CNF_APPNAME','Gestión Integral de Servicios');
+define('CNF_APPDESC','CRM');
+define('CNF_EMPID','1');//DE ACUERDO A BDD
+define('CNF_COMNAME','APPgency');
+define('CNF_EMAIL','info@mycompanyname.com');
+define('CNF_METAKEY','my site , my company  , Larvel Crud');
+define('CNF_METADESC','Write description for your site');
+define('CNF_GROUP','3');
+define('CNF_ACTIVATION','confirmation');
+define('CNF_MULTILANG','0');
+define('CNF_LANG','es');
+define('CNF_REGIST','true');
+define('CNF_FRONT','true');
+define('CNF_RECAPTCHA','false');
+define('CNF_THEME','porto');
+define('CNF_RECAPTCHAPUBLICKEY','');
+define('CNF_RECAPTCHAPRIVATEKEY','');
+define('CNF_MODE','production');
+define('CNF_LOGO','backend-logo.png');
+define('CNF_ALLOWIP','');
+define('CNF_RESTRICIP','192.116.134 , 194.111.606.21 ');
+?>
